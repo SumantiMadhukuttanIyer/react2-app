246e2ba8233160cb5f773ac9e79b660a1ee95dae
SUMANTI <SUMANTI@DESKTOP-S8MO377>
2022-05-26 19:45:22 +0530

    react2

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,11 +6,11 @@ const AddUser = (props) => {
     }
     return(
     <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
+        <label htmlFor="username">Customer Name</label>
         <input id="username" type="text" />
-        <label htmlFor="age">Age (Years)</label>
+        <label htmlFor="age">Customer Age</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <button type="submit">Enter Data</button>
     </form>
     );
 };
